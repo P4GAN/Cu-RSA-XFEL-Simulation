89384725a35c39818bb3addbb1f7d2ad554ee226
P4GAN <parkinpham@gmail.com>
2026-08-04 16:27:21 +0200

    Optimize einsum in MB_nlevel_regular

--- example_script.py
+++ example_script.py
@@ -3,13 +3,13 @@ import time
 from XLO_sim.XLO_sim import XLO_sim
 from XLO_sim import tools
 
-n_repetitions = 1
+n_repetitions = 10
 
 start_time = time.perf_counter()
 
 for i in range(n_repetitions):
     print(f"Repetition {i+1}/{n_repetitions}")
-    X = XLO_sim("/Users/parkinpham/Programming/Physics/DESY_Internship/XLO_sim_related_notebook/config/Cu-seed_40.00uJ.yaml")
+    X = XLO_sim("/Users/parkinpham/Programming/Physics/DESY_Internship/XLO_sim_related_notebook/config/Cu-seed.yaml")
 
     seed_field = tools.Gaussian_pulse_aniso_seed(X)
     X.configure(seed_field)

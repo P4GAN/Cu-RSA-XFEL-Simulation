964aeda7813731285073a13e77abeeb56b52ce66
P4GAN <parkinpham@gmail.com>
2026-08-04 12:34:09 +0200

    Update profiling script

--- example_script.py
+++ example_script.py
@@ -1,44 +1,15 @@
-import os
-notebook_path = os.path.abspath("__file__")
-notebook_directory = os.path.dirname(notebook_path)
-XLO_path = notebook_directory + '/XLO_sim/'
-mini_ocelot_path = notebook_directory + '/mini_ocelot/'
+import time
 
-import sys
-sys.path.append(XLO_path)
-sys.path.append(mini_ocelot_path)
+from XLO_sim.XLO_sim import XLO_sim
+from XLO_sim import tools
 
-import warnings
-
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib
-from XLO_sim import *
-import Plot as XLO_plot
-
-
-from numpy import random
-from numpy.linalg import norm
-from math import factorial
-from numpy import inf, complex128, complex64
-import scipy
-import numpy.fft as fft
-from copy import deepcopy
-
-
-from o_globals import *
-from math_op import find_nearest_idx, fwhm, std_moment, bin_scale, bin_array, mut_coh_func
-from py_func import filename_from_path
-from ocelog import *
-from new_wave import *
-import scipy.constants as sp_const
-au_in_eV = sp_const.value('atomic unit of energy') / sp_const.value('atomic unit of charge')
-
-import tools
-
+start_time = time.perf_counter()
 
 X = XLO_sim("/Users/parkinpham/Programming/Physics/DESY_Internship/XLO_sim_related_notebook/config/Cu-seed_40.00uJ.yaml")
 
 seed_field = tools.Gaussian_pulse_aniso_seed(X)
 X.configure(seed_field)
 X.run_3D()
+
+elapsed_time = time.perf_counter() - start_time
+print(f"Execution took {elapsed_time:.4f} seconds")

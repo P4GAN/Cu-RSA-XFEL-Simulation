a50cb154ffc669daae3a510c99b425c7ae508ac7
P4GAN <parkinpham@gmail.com>
2026-08-04 13:55:17 +0200

    update

--- example_script.py
+++ example_script.py
@@ -3,13 +3,17 @@ import time
 from XLO_sim.XLO_sim import XLO_sim
 from XLO_sim import tools
 
+n_repetitions = 5
+
 start_time = time.perf_counter()
 
-X = XLO_sim("/Users/parkinpham/Programming/Physics/DESY_Internship/XLO_sim_related_notebook/config/Cu-seed_40.00uJ.yaml")
+for i in range(n_repetitions):
+    print(f"Repetition {i+1}/{n_repetitions}")
+    X = XLO_sim("/Users/parkinpham/Programming/Physics/DESY_Internship/XLO_sim_related_notebook/config/Cu-seed_40.00uJ.yaml")
 
-seed_field = tools.Gaussian_pulse_aniso_seed(X)
-X.configure(seed_field)
-X.run_3D()
+    seed_field = tools.Gaussian_pulse_aniso_seed(X)
+    X.configure(seed_field)
+    X.run_3D()
 
 elapsed_time = time.perf_counter() - start_time
 print(f"Execution took {elapsed_time:.4f} seconds")

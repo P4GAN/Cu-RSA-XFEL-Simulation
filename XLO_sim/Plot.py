Fix sweeps, restructure notebooks and remove unneccesary calculation

XLO_sim/Plot.py
@@ -1185,7 +1185,10 @@ class XLO_plot:
             I_t = np.einsum('stxy,stxy->t', X.Omega_pstxyz[0, :, :, :, :, zint], X.Omega_pstxyz[1, :, :, :, :, zint])
             E_t = np.einsum('ijs, jkt, kis-> t', X.Tijs_minus, X.rho_ijtxyz[:,:,:,int(X.xgrid/2),int(X.ygrid/2),zint], X.Tijs_plus)
             G_t = np.einsum('ijs, jkt, kis-> t', X.Tijs_plus, X.rho_ijtxyz[:,:,:,int(X.xgrid/2),int(X.ygrid/2),zint], X.Tijs_minus)
-            P_t = X.P_pstxyz[0,0,:,int(X.xgrid/2),int(X.ygrid/2),zint]
+
+            P_pstxyz = np.array([ np.einsum('ijs,jitxyz->stxyz', self.Tijs_minus, self.rho_ijtxyz) , np.einsum('ijtxyz,jis->stxyz', self.rho_ijtxyz, self.Tijs_plus) ])
+
+            P_t = P_pstxyz[0,0,:,int(X.xgrid/2),int(X.ygrid/2),zint]
             # j_t = X.j_3D[:,int(X.xgrid/2),int(X.ygrid/2),zint]
             r0_t = X.rho_0_3D[:,int(X.xgrid/2),int(X.ygrid/2),zint]
             t_axis = X.t

XLO_sim/XLO_sim.py
@@ -300,31 +300,9 @@ class XLO_sim:
         self.nphoton_pump_z = tools.nphoton_pump_z(self)
         if self.nphoton_pump_z[0] != 0.0:
             self.pump_transmission = self.nphoton_pump_z[-1] / self.nphoton_pump_z[0]
-        
-        self.E_sstxyz = np.einsum('ijp,jktxyz,kir->prtxyz', self.Tijs_minus, self.rho_ijtxyz, self.Tijs_plus, optimize=True)
-        self.G_sstxyz = np.einsum('ijr,jktxyz,kip->prtxyz', self.Tijs_plus, self.rho_ijtxyz, self.Tijs_minus, optimize=True)
-
-        self.P_pstxyz = np.array([ np.einsum('ijs,jitxyz->stxyz', self.Tijs_minus, self.rho_ijtxyz) , np.einsum('ijtxyz,jis->stxyz', self.rho_ijtxyz, self.Tijs_plus) ])
-        
+      
         if self.is_Cartesian_pol:
             Omega_pqtxy = tools.circular_to_linear(self, self.Omega_pstxyz[:, :, :, :, :, -1])
             self.Omega_qtxy = (Omega_pqtxy[0, :, :, :, :] + np.conj(Omega_pqtxy[1, :, :, :, :])) / 2.0
             
         self.Pfield_txyz = self.sample.Pfield_txyz
-
-        
-    def pre_run_3D(self):
-        """
-        Calculate ASE field propagation and density matrix evolution with or without absorption of the ASE field.
-
-        Returns
-        -------
-
-        """
-
-        self.sample.evaluate_rho_ij_with_pump_3D(self)
-
-        self.pre_rho_ijtxyz = self.sample.rho_ijtxyz
-        self.Omega_pstxyz = self.sample.Omega_pstxyz
-
-        self.P_pstxyz = np.array([ np.einsum('ijs,jitxyz->stxyz', self.Tijs_minus, self.pre_rho_ijtxyz) , np.einsum('ijtxyz,jis->stxyz', self.pre_rho_ijtxyz, self.Tijs_plus) ])

scripts/run_mono_transmittance_sweep.py
@@ -59,15 +59,29 @@ def run_simulation(yaml_path, run_path, rep):
     womega_ar, I_int_thy_w_last, I_thy0_w_last = tools.SF_spectrum_w(X, -1, YPAD, TPAD)
 
     target_energy_eV = X.monochromator_target_energy_eV
+    P_pstxyz = np.array([ np.einsum('ijs,jitxyz->stxyz', X.Tijs_minus, X.rho_ijtxyz) , np.einsum('ijtxyz,jis->stxyz', X.rho_ijtxyz, X.Tijs_plus) ])
+    
+    I_t_0 = np.einsum('stxy,stxy->t', X.Omega_pstxyz[0, :, :, :, :, 0], X.Omega_pstxyz[1, :, :, :, :, 0])
+    I_t_last = np.einsum('stxy,stxy->t', X.Omega_pstxyz[0, :, :, :, :, -1], X.Omega_pstxyz[1, :, :, :, :, -1])
+    rho_ee_t_last = np.einsum('ijs, jkt, kis-> t', X.Tijs_minus, X.rho_ijtxyz[:,:,:,int(X.xgrid/2),int(X.ygrid/2),-1], X.Tijs_plus)
+    rho_gg_t_last = np.einsum('ijs, jkt, kis-> t', X.Tijs_plus, X.rho_ijtxyz[:,:,:,int(X.xgrid/2),int(X.ygrid/2),-1], X.Tijs_minus)
+    rho_eg_t_last = P_pstxyz[0,0,:,int(X.xgrid/2),int(X.ygrid/2),-1]
+    rho_ground_t_last = X.rho_0_3D[:,int(X.xgrid/2),int(X.ygrid/2),-1]
+    t_axis = X.t
+
     date_string = np.datetime_as_string(np.datetime64('now'))
     np.savez_compressed(
         os.path.join(run_path, f"run_at_seed_{X.E_seed_uJ:.1f}_uJ__energy_{target_energy_eV:.2f}_eV__repetition_{rep + 1}_{date_string}.npz"),
-        target_energy_eV=target_energy_eV,
         womega_ar=womega_ar,
         I_int_thy_w_0=I_int_thy_w_0,
-        I_thy0_w_0=I_thy0_w_0,
         I_int_thy_w_last=I_int_thy_w_last,
-        I_thy0_w_last=I_thy0_w_last,
+        I_t_0=I_t_0,
+        I_t_last=I_t_last,
+        rho_ee_t_last=rho_ee_t_last,
+        rho_gg_t_last=rho_gg_t_last,
+        rho_eg_t_last=rho_eg_t_last,
+        rho_ground_t_last=rho_ground_t_last,
+        t_axis=t_axis
     )
 
 